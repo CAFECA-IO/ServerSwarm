a998d6bbd042403b9d61b76056a841e696caedb7
RexBearIU <rexbear87941@gmail.com>
2024-09-02 18:03:59 +0800

    feat: private ipfs

---
+++ ipfs/ipfs-start.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]' 
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+
+# Info: (20240902 - Jacky) Ensure the environment variable is set
+if [ -z "$SWARM_KEY" ]; then
+  echo "KEY environment variable is not set."
+  exit 1
+fi
+
+# Info: (20240902 - Jacky) Create the file in /data/ipfs directory
+cat <<EOF > /data/ipfs/swarm.key
+/key/swarm/psk/1.0.0/
+/base16/
+$SWARM_KEY
+EOF
+
+echo "File created successfully at /data/ipfs/swarm.key"
+
+# Info: (20240902 - Jacky) Clean up the bootstrap list
+ipfs bootstrap rm --all
+
+# Info: (20240902 - Jacky) Ensure the environment variable is set
+if [ -z "$BOOTSTRAP_NODES" ]; then
+  echo "BOOTSTRAP_NODES environment variable is not set."
+  exit 1
+fi
+
+# Info: (20240902 - Jacky) Add the bootstrap nodes
+for node in $BOOTSTRAP_NODES; do
+  echo "Adding bootstrap node: $node"
+  ipfs bootstrap add "$node"
+done
+
+echo "All bootstrap nodes added successfully."
+
+ipfs daemon
