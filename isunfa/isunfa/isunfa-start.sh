87d464bcaae77344b74c4fbf59b4ac0746233aa6
arealclimber <luffulgence@gmail.com>
2024-10-15 16:40:37 +0800

    feat: 🎸 change of config of postgres, revise docker compose, co
    -mmand of isunfa start sh

--- isunfa/isunfa/isunfa-start.sh
+++ isunfa/isunfa/isunfa-start.sh
@@ -3,7 +3,8 @@
 # Info: (20240821 - Jacky) 嘗試克隆 Git 倉庫到 /isunfa/app 目錄
 # 如果目錄已經存在，則會失敗並執行後面的命令
 # git clone https://github.com/CAFECA-IO/iSunFA.git -b v0.8.0 --single-branch /isunfa/app || (cd /isunfa/app && git pull origin main)
-git clone https://github.com/CAFECA-IO/iSunFA.git /isunfa/app || (cd /isunfa/app && git pull origin main)
+# git clone https://github.com/CAFECA-IO/iSunFA.git /isunfa/app || (cd /isunfa/app && git pull origin main)
+git clone https://github.com/CAFECA-IO/iSunFA.git /isunfa/app || (cd /isunfa/app && git pull origin develop)
 
 # Info: (20240821 - Jacky) 進入 /isunfa/app 目錄並安裝依賴（假設是 Node.js 應用）
 cd /isunfa/app && npm install
@@ -14,7 +15,9 @@ npm install --global pm2
 # Info: (20240821 - Jacky) 執行 Prisma 相關命令
 npx prisma migrate deploy
 
-npx prisma db seed
+# Info: (20241011 - Shirley) 使用 prisma db seed 會出現 `Error: Command failed with exit code 1: ts-node -r tsconfig-paths/register --compiler-options {"module":"CommonJS"} prisma/seed.ts` 錯誤
+# npx prisma db seed
+npx ts-node -r tsconfig-paths/register --compiler-options '{"module":"CommonJS"}' prisma/seed.ts
 
 # Info: (20240821 - Jacky) 執行構建命令
 npm run build
